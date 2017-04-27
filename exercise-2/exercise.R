9bf3178286731564e824bf82b9833b097e55d336
Lee Segal <leesegal6@gmail.com>
2017-04-27 14:45:26 -0700

    practice

--- exercise-2/exercise.R
+++ exercise-2/exercise.R
@@ -1,15 +1,26 @@
 ### Exercise 2 ###
 
 # Load the httr and jsonlite libraries for accessing data
+library("httr")
+library("jsonlite")
 
 
 # Create a `base.uri` variable that holds the base uri. You wil then paste endpoints to this base.
+base.uri <- "https://api.spotify.com"
 
 
 
 ## As you may have noticed, it often takes multiple queries to retrieve the desired information.
 ## This is a perfect situation in which writing a function will allow you to better structure your
 ## code, and give a name to a repeated task!
+resource <- "/v1/search"
+uri = paste0(base.uri, resource)
+query.params <- list(q = artist, type = "artist")
+
+response <- GET(uri, query = query.params)
+
+artist.info <- fromJSON(content(response, "text"))
+artist.id <- artist.info$artists$items$id[1]
 
 
 # Define a function `TopTrackSearch` that takes in an artist name as an argument,
